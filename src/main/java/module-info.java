eb2353f37d2392e0c59140583e3383fa545c2832
RezzedUp <RezzedUp@users.noreply.github.com>
2021-04-25 01:32:00 -0500

    Add license headers.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2021, RezzedUp <https://github.com/RezzedUp/Constants>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 module com.rezzedup.util.constants
 {
     requires static pl.tlinkowski.annotation.basic;
