b93baefbd0f8752983f3213a9dff7c45b6a58c14
RezzedUp <RezzedUp@users.noreply.github.com>
2022-01-01 12:06:42 -0600

    Update copyright to 2022.

--- src/main/java/com/rezzedup/util/constants/types/TypeCompatible.java
+++ src/main/java/com/rezzedup/util/constants/types/TypeCompatible.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021, RezzedUp <https://github.com/RezzedUp/Constants>
+ * Copyright © 2021-2022, RezzedUp <https://github.com/RezzedUp/Constants>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
